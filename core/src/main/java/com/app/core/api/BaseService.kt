5674ca38201ccfaefb223e2f00ef1b53f9a61d48
anilkumargudigar <anil.kumar@daytoday.health>
2020-06-07 19:41:23 +0530

    arch changes

---
+++ core/src/main/java/com/app/core/api/BaseService.kt
@@ -0,0 +1,9 @@
+package com.app.core.api
+
+interface BaseService {
+
+    companion object {
+        const val ENDPOINT = "https://api.themoviedb.org/3/"
+        const val IMAGE_ENDPOINT = "https://image.tmdb.org/t/p/w1280"
+    }
+}
